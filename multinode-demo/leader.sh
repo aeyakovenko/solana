9e88e708dab5d8a6056b7c6cf0977cefef0a6b1e
Anatoly Yakovenko <anatoly@solana.com>
2018-06-07 22:02:08 +0000

    terrible scripts

--- multinode-demo/leader.sh
+++ multinode-demo/leader.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 # if RUST_LOG is unset, default to info
 export RUST_LOG=${RUST_LOG:-solana=info}
 
 set -x
 [[ $(uname) = Linux ]] && sudo sysctl -w net.core.rmem_max=26214400
 
-cargo run --release --bin solana-fullnode -- \
+cargo run --release --bin solana-fullnode --features=cuda -- \
       -l leader.json < genesis.log tx-*.log > tx-"$(date -u +%Y%m%d%k%M%S%N)"
