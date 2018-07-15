233632909cc74a60e7c80c4bdf3b340ec62a2731
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-07-14 20:11:47 -0700

    wip

--- src/page_table.rs
+++ src/page_table.rs
@@ -863,7 +863,7 @@ mod test {
     }
     #[test]
     fn load_and_execute_bench_test() {
-        const T: usize = 16;
+        const T: usize = 32;
         let pt = PageTable::new();
         let count = 1000;
         let mut ttx: Vec<Vec<Call>> = (0..count)
